918f0ede941ae28247b78733a121a6998ea0e15b
baejunil <bjo6300@naver.com>
2022-11-01 00:53:13 +0900

    chore : 비즈니스 요구사항 정리, 회원 도메인과 리포지토리 만들기

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+public class MemoryMemberRepository implements  MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // id, Member 저장하는 해쉬맵
+    private static long sequence = 0L; // id
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // Optional로 감싸면 클라이언트에서 처리가능
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> finaAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
