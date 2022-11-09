12d2e723df7dc97e87cd3895cd7496a67fbd345a
baejunil <bjo6300@naver.com>
2022-11-10 01:11:14 +0900

    feat : 회원 관리 예제 - 웹 MVC 개발
    회원 웹 기능 - 홈 화면 추가 ~ 회원 웹 기능 - 조회

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,6 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 스프링이 뜰 때 컨트롤러 객체를 가지고 있다. -> 스프링 컨테이너에서 스프링 빈이 관리된다.
 public class MemberController {
@@ -21,5 +26,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm"; // template으로 이동
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName()); // html form에서 받은걸 member의 name에 set
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
     }
 }
