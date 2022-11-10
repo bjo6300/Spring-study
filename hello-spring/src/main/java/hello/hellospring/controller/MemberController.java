aad8c1d1b11965383228093b73a5818318d35cae
baejunil <bjo6300@naver.com>
2022-11-10 23:33:57 +0900

    feat : AOP 적용

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -26,6 +26,7 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+//        System.out.println("memberService = " + memberService.getClass()); // 실제 proxy가 주입되는지 콘솔에 출력해서 확인
     }
 
     @GetMapping("/members/new")
